c81d32773f8779e1096fed46486d7dc51d7265a4
dev-welligtonlins <welligtonlinsemp@gmail.com>
2025-08-11 14:11:50 -0300

    Corrigir settings.py 3

--- portal/settings.py
+++ portal/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',') if os.getenv('ALLOWED_HOSTS') else []
 
 
 # Application definition
